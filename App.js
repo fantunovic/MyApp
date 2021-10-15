4bccd0bdccac8b6aed02cecf17f907891490ec87
fantunovic <91869005+fantunovic@users.noreply.github.com>
2021-10-15 20:30:03 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Alert, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert, Button, TouchableOpacity, Linking } from 'react-native';
+
 
 export default function App() {
   return (
@@ -8,36 +9,56 @@ export default function App() {
 
       <View style={styles.headerWrapper}>
         <Text style={styles.headerTitle}>
-          Test app header fdfdfdf
+          MOJA APLIKACIJA
         </Text>
       </View>
 
       <View style={styles.jumbotron}>
         <View style={styles.leftContainer}>
-          <Text style={styles.leftText}>LEFT</Text>
+        <Image style={styles.image} source={require("./assets/1.jpg")}/>
         </View>
         <View style={styles.rightContainer}>
-          <Text style={styles.rightText}>RIGHT</Text>
+        <Image style={styles.image} source={require("./assets/2.jpg")}/>
         </View>
       </View>
-
-      <Image style={styles.image} source={{uri:"https://pbs.twimg.com/profile_images/1222824625903558657/NCjFSJkL_400x400.jpg"}}/>
-
-      
 
       <View style={styles.jumbotron}>
         <View style={styles.leftContainer}>
-        <Image style={styles.image} source={require("./assets/favicon.png")}/>
+          <Button color="#6a994e" title="Livada" onPress={() => Alert.alert("Ovo je livada")}/>
         </View>
         <View style={styles.rightContainer}>
-        <Image style={styles.image} source={require("./assets/test.jpg")}/>
+          <Button color="#6a994e" title="Šuma" onPress={() => Alert.alert("Ovo je šuma")}/>
         </View>
       </View>
 
-      <Button title="Naslov buttona" onPress={() => Alert.alert("Button pressed")}/>
+      <View style={styles.jumbotron}>
+        <View style={styles.leftContainer}>
+        <Image style={styles.image} source={require("./assets/3.jpg")}/>
+        </View>
+        <View style={styles.rightContainer}>
+        <Image style={styles.image} source={require("./assets/4.jpg")}/>
+        </View>
+      </View>
 
-      <TouchableOpacity style={styles.touchableCountainer} onPress={() => Alert.alert("Touchable opacity pressed")}>
-      <Text style={styles.textStyle}>Touchable opacity</Text>
+      <View style={styles.jumbotron}>
+        <View style={styles.leftContainer}>
+          <Button color="#6a994e" title="Jezero" onPress={() => Alert.alert("Ovo je jezero")}/>
+        </View>
+        <View style={styles.rightContainer}>
+          <Button color="#6a994e" title="Rijeka" onPress={() => Alert.alert("Ovo je rijeka")}/>
+          
+
+        </View>
+      </View>
+
+    
+
+      
+
+     
+
+      <TouchableOpacity style={styles.touchableCountainer} onPress={() => Alert.alert("Valjda je to sve")}>
+      <Text style={styles.textStyle}>KRAJ APLIKACIJE</Text>
       </TouchableOpacity>
 
     </View>
@@ -52,70 +73,78 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#A8B4A5',
+    backgroundColor: '#a7c957',
 
 
   },
 
   headerWrapper: {
     padding: 10,
-    borderBottomWidth: 2
+    margin: 30,
+    borderBottomWidth: 5,
+    borderBottomColor: "white"
   },
   headerTitle: {
     textAlign: "center",
-    fontSize: 30
+    fontSize: 35,
+    color: "white",
+   
   },
 
   jumbotron: {
     flexDirection: "row",
     justifyContent: "space-between",
-    
-
+    padding: 10
   },
 
   leftContainer: {
  
-    backgroundColor: 'grey',
+    
 
   },
 
   rightContainer: {
  
-    backgroundColor: 'white',
+    
 
   },
 
   rightContainer: {
  
-    backgroundColor: 'white',
+    
 
   },
 
   leftText: {
-    color: "red",
-    fontSize: 30
+    color: "white",
+    fontSize: 25,
+    
 
   },
 
   rightText: {
- 
+    fontSize: 25,
+    color: "white",
 
   },
 
   image: {
-    width: 100,
-    height:100
+    width: 175,
+    height:175
 
   },
 
   touchableCountainer: {
     margin: 20,
-    backgroundColor: "white",
+    backgroundColor: "#6a994e",
     padding: 20
   },
 
   textStyle: {
-    fontSize: 20
-  }
+    fontSize: 20,
+    color: "white",
+    textAlign: "center"
+  },
+
 
 });
